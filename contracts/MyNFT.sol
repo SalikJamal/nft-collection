28b93dcaa7fb31b2040c36fb7a74c26be9560805
Salik <iamsalikjamal@gmail.com>
2022-08-09 20:45:50 +0500

    Setting up project

---
+++ contracts/MyNFT.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
+
+
+import 'hardhat/console.sol';
+
+
+contract MyNFT {
+
+    constructor() {
+        console.log('Get The F Out Of Localhost!');
+    }
+
+}
---
+++ scripts/run.js
@@ -0,0 +1,26 @@
+const hre = require("hardhat");
+
+
+const main = async () => {
+
+    const nftContractFactory = await hre.ethers.getContractFactory('MyNFT')
+    const nftContract = await nftContractFactory.deploy()
+    await nftContract.deployed()
+
+    console.log('Contract Deployed to: ', nftContract.address)
+
+}
+
+const runMain = async () => {
+
+    try {
+        await main()
+        process.exit(0)
+    } catch(e) {
+        console.log(e)
+        process.exit(1)
+    }
+
+}
+
+runMain()
