7022c54810861d8f2f1486c82f0ca472ab3b060a
Salik <iamsalikjamal@gmail.com>
2022-08-19 22:34:58 +0500

    updated nft metadata

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -29,7 +29,7 @@ contract MyNFT is ERC721URIStorage {
         _safeMint(msg.sender, newItemId);
 
         // Set the NFTs data.
-        _setTokenURI(newItemId, 'https://jsonkeeper.com/b/ULD4');
+        _setTokenURI(newItemId, 'data:application/json;base64,ewogICAgIm5hbWUiOiAiQ29TaWduMTgiLAogICAgImRlc2NyaXB0aW9uIjogJ09uZSBvZiB0aGUgTkZUcyBvZiBhbGwgb25saW5lIGFsaWFzZXMgb2YgIkNvU2lnbjE4IicsCiAgICAiaW1hZ2UiOiAiZGF0YTppbWFnZS9zdmcreG1sO2Jhc2U2NCxQSE4yWnlCNGJXeHVjejBpYUhSMGNEb3ZMM2QzZHk1M015NXZjbWN2TWpBd01DOXpkbWNpSUhCeVpYTmxjblpsUVhOd1pXTjBVbUYwYVc4OUluaE5hVzVaVFdsdUlHMWxaWFFpSUhacFpYZENiM2c5SWpBZ01DQXpOVEFnTXpVd0lqNEtJQ0E4Y21WamRDQjNhV1IwYUQwaU1UQXdKU0lnYUdWcFoyaDBQU0l4TURBbElpOCtDaUFnUEhSbGVIUWdlRDBpTlRBbElpQjVQU0kxTUNVaUlHUnZiV2x1WVc1MExXSmhjMlZzYVc1bFBTSnRhV1JrYkdVaUlIUmxlSFF0WVc1amFHOXlQU0p0YVdSa2JHVWlJSE4wZVd4bFBTSm1hV3hzT2lObVptWTdabTl1ZEMxbVlXMXBiSGs2YzJWeWFXWTdabTl1ZEMxemFYcGxPakUwY0hnaVBrTnZVMmxuYmpFNFBDOTBaWGgwUGdvOEwzTjJaejRLIgp9');
 
         console.log('An NFT w/ ID %s has been minted to %s', newItemId, msg.sender);
 
